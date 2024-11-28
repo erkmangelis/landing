e163914c74d44a67c51e20d757ac90cf9457a03a
Erkman Geliş <erkmangls@hotmail.com>
2024-11-28 14:52:18 +0300

    Initial Create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,89 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button, Divider } from 'antd';
+import { InstagramOutlined, LinkedinOutlined, GithubOutlined, GoogleOutlined } from '@ant-design/icons';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const audio = new Audio('/sound.mp3');
+    audio.loop = true;
+    audio.play();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1 className='typewriter'>Demir İşleniyor<span className="dots"></span></h1>
+        <h4>Bu dijital zanaata tanıklık edin; yakında görüşmek üzere.</h4>
+      </header>
+
+      <main>
+        <div className='container'>
+          <Image
+              className='fade'
+              src="/circle.gif"
+              height={400}
+              width={400}
+              alt='asd'
+              unoptimized
+              draggable="false"
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer>
+        <div className="buttonWrapper">
+          <Link href="https://www.instagram.com/erkmangelis/" passHref>
+            <Button style={{color: '#ffcc66a4'}} type="link" icon={<InstagramOutlined />} size="large"/>
+          </Link>
+          <Divider type="vertical" style={{ borderColor: '#ffcc66a4' }} />
+          <Link href="https://www.linkedin.com/in/erkmangelis/" passHref>
+            <Button style={{color: '#ffcc66a4'}} type="link" icon={<LinkedinOutlined />} size="large"/>
+          </Link>
+          <Divider type="vertical" style={{ borderColor: '#ffcc66a4' }} />
+          <Link href="https://github.com/erkmangelis/" passHref>
+            <Button style={{color: '#ffcc66a4'}} type="link" icon={<GithubOutlined />} size="large"/>
+          </Link>
+          <Divider type="vertical" style={{ borderColor: '#ffcc66a4' }} />
+          <Link href="mailto:erkmangls@gmail.com" passHref>
+            <Button style={{color: '#ffcc66a4'}} type="link" icon={<GoogleOutlined />} size="large"/>
+          </Link>
+        </div>
+        <div className='copyright'>
+          <p>© 2024 Erkmen Geliş. Tüm hakları saklıdır.</p>
+        </div>
       </footer>
+
+      <div className="particle-container">
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
     </div>
+  </>
   );
 }

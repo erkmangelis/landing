5e4784773e05f437fe698441d1cce9a563a7906e
Erkman Geliş <88348596+erkmangelis@users.noreply.github.com>
2024-11-28 23:28:35 +0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,53 @@
 "use client"
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button, Divider } from 'antd';
-import { InstagramOutlined, LinkedinOutlined, GithubOutlined, MailOutlined } from '@ant-design/icons';
+import { InstagramOutlined, LinkedinOutlined, GithubOutlined, MailOutlined, SoundOutlined } from '@ant-design/icons';
 
 export default function Home() {
+  const [audioPlaying, setAudioPlaying] = useState(false); // Ses durumu
+  const [gifKey, setGifKey] = useState(Date.now()); // GIF'in sıfırdan başlaması için key kullanıyoruz
+
+  // Sayfa yüklendiğinde otomatik ses çalma
   useEffect(() => {
+    // Sayfa yüklendiğinde ses başlatma
     const audio = new Audio('/sound.mp3');
     audio.loop = true;
-    audio.play();
+    audio.play().then(() => {
+      setAudioPlaying(true); // Ses başladığında durumu güncelle
+    }).catch(err => {
+      console.error("Ses çalarken bir hata oluştu: ", err);
+    });
 
+    // GIF'i sıfırdan başlatmak için key'i değiştirelim
+    setGifKey(Date.now());
+
+    // Temizlik
     return () => {
-      audio.pause();
-      audio.currentTime = 0;
+      audio.pause(); // Sayfa kapanırken sesi durdur
     };
-  }, []);
+  }, []); // Boş array ile sadece bir kez çalışmasını sağlıyoruz
+
+  // Ses çalmaya başlandığında GIF'i sıfırdan başlatmak için
+  const handleAudioStart = () => {
+    if (!audioPlaying) {
+      const audio = new Audio('/sound.mp3');
+      audio.loop = true;
+      audio.play();
+      setAudioPlaying(true);
+
+      // GIF'i sıfırdan başlatmak için key'i değiştirelim
+      setGifKey(Date.now());
+    }
+  };
 
   return (
     <>
+      {!audioPlaying && (
+      <Button onClick={handleAudioStart} style={{backgroundColor: '#ffcc66a4', color: 'rgba(50, 40, 40, 1)', position: 'fixed', margin: '10px'}} shape="circle" type="primary" icon={<SoundOutlined />} size="large"/>
+      )}
       <header>
         <h1 className='typewriter'>Demir İşleniyor<span className="dots"></span></h1>
         <h4>Bu dijital zanaata tanıklık edin; yakında görüşmek üzere.</h4>
@@ -28,6 +56,7 @@ export default function Home() {
       <main>
         <div className='container'>
           <Image
+              key={gifKey}
               className='fade'
               src="/circle.gif"
               height={400}
